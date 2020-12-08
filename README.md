b58fa564d71f9f2874432a919474f8a94e38eb30
David Burkett <david@davidburkett.xyz>
2020-12-07 20:26:38 -0500

    Finished Day 7

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ Repository of my solutions to [Advent of Code 2020](https://adventofcode.com/202
 | Day 4  | [Day4.hs](app/Day4.hs) |
 | Day 5  | [Day5.hs](app/Day5.hs) |
 | Day 6  | [Day6.hs](app/Day6.hs) |
+| Day 7  | [Day7.hs](app/Day7.hs) |
--- app/Day7.hs
+++ app/Day7.hs
@@ -1,4 +1,75 @@
 module Main where
 
+import AdventOfCode.Parser (parseLinesOfFile)
+import Data.Attoparsec.Text (Parser)
+import qualified Data.Attoparsec.Text as Parser
+import Data.Functor (($>))
+import Data.List (foldl')
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import Numeric.Natural (Natural)
+
+data Bag = Bag
+  { bagAdjective :: !Text,
+    bagColor :: !Text
+  }
+  deriving (Show, Eq, Ord)
+
+lineParser :: Parser (Bag, [(Natural, Bag)])
+lineParser =
+  let bagParser = do
+        bagAdjective <- Parser.takeTill (== ' ')
+        _ <- Parser.space
+        bagColor <- Parser.takeTill (== ' ')
+        _ <- Parser.space *> Parser.string "bag"
+        Parser.option () (Parser.char 's' $> ())
+        pure $ Bag {..}
+      noOtherBagsParser =
+        Parser.string "no other bags" $> []
+      innerBagParser = (,) <$> (Parser.decimal <* Parser.space) <*> bagParser
+      innerBagsParser =
+        Parser.choice
+          [ noOtherBagsParser,
+            Parser.sepBy1 innerBagParser (Parser.string ", " $> ())
+          ]
+   in do
+        bag <- bagParser
+        _ <- Parser.string " contain "
+        innerBags <- innerBagsParser
+        _ <- Parser.char '.'
+        pure (bag, innerBags)
+
+getDay7Input :: IO (Map Bag [(Natural, Bag)])
+getDay7Input =
+  Map.fromList <$> parseLinesOfFile "data/day7" lineParser
+
+shinyGold :: Bag
+shinyGold = Bag "shiny" "gold"
+
+part1 :: Map Bag [(Natural, Bag)] -> Natural
+part1 input =
+  let hasShinyGold bag =
+        case fmap snd <$> Map.lookup bag input of
+          Nothing -> False
+          Just bags -> shinyGold `elem` bags || any hasShinyGold bags
+      possibleOuterBags = filter (/= shinyGold) $ Map.keys input
+   in fromIntegral . length $ filter hasShinyGold possibleOuterBags
+
+part2 :: Map Bag [(Natural, Bag)] -> Natural
+part2 input =
+  let bagsInsideShinyGold = fromMaybe [] $ Map.lookup shinyGold input
+      sum' = foldl' @[] @Natural (+) 0
+      countInnerBags (count, bag) =
+        case Map.lookup bag input of
+          Nothing -> 0
+          Just [] -> count
+          Just moreBags -> count + count * sum' (countInnerBags <$> moreBags)
+   in sum' $ fmap countInnerBags bagsInsideShinyGold
+
 main :: IO ()
-main = putStrLn "pending"
+main = do
+  input <- getDay7Input
+  putStrLn $ "Part 1: " <> show (part1 input)
+  putStrLn $ "Part 2: " <> show (part2 input)

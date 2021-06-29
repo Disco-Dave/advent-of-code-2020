ae2b81a14d2c677b13ed08a288efc795717831be
David Burkett <david@davidburkett.xyz>
2021-06-28 21:33:38 -0400

    Completed part 1 of day 11

--- src/AdventOfCode/Solutions/Day11.hs
+++ src/AdventOfCode/Solutions/Day11.hs
@@ -1,4 +1,76 @@
 module AdventOfCode.Solutions.Day11 where
 
+import AdventOfCode.Parser (parseLinesOfFile)
+import Data.Attoparsec.Text (Parser)
+import qualified Data.Attoparsec.Text as Parser
+import Data.Functor (($>), (<&>))
+import Data.Vector (Vector, (!?))
+import qualified Data.Vector as Vector
+
+data Seat
+  = EmptySeat
+  | TakenSeat
+  deriving (Show, Eq)
+
+data Tile
+  = EmptyTile
+  | SeatTile Seat
+  deriving (Show, Eq)
+
+type Seating = Vector (Vector Tile)
+
+tileParser :: Parser Tile
+tileParser =
+  Parser.choice
+    [ Parser.char '.' $> EmptyTile
+    , Parser.char 'L' $> SeatTile EmptySeat
+    , Parser.char '#' $> SeatTile TakenSeat
+    ]
+
+getDay11Input :: IO Seating
+getDay11Input =
+  Vector.fromList
+    <$> parseLinesOfFile
+      "data/day11"
+      (fmap Vector.fromList (Parser.many1 tileParser))
+
+simulate :: Seating -> Seating
+simulate seating =
+  let countAdjacentOccupiedSeats rowIndex columnIndex =
+        let adjacentSeats =
+              [ (rowIndex - 1, columnIndex - 1)
+              , (rowIndex - 1, columnIndex)
+              , (rowIndex - 1, columnIndex + 1)
+              , (rowIndex, columnIndex - 1)
+              , (rowIndex, columnIndex + 1)
+              , (rowIndex + 1, columnIndex - 1)
+              , (rowIndex + 1, columnIndex)
+              , (rowIndex + 1, columnIndex + 1)
+              ]
+                <&> \(r, c) -> seating !? r >>= (!? c)
+         in length $ filter (== Just (SeatTile TakenSeat)) adjacentSeats
+      simulateSeat rowIndex columnIndex = \case
+        EmptyTile -> EmptyTile
+        SeatTile seat ->
+          let occupiedAdjacentSeats = countAdjacentOccupiedSeats rowIndex columnIndex
+           in SeatTile $ case seat of
+                EmptySeat | occupiedAdjacentSeats == 0 -> TakenSeat
+                TakenSeat | occupiedAdjacentSeats >= 4 -> EmptySeat
+                _ -> seat
+   in Vector.imap (Vector.imap . simulateSeat) seating
+
+part1 :: Seating -> Int
+part1 =
+  let stabalize seating
+        | seating == newSeating = seating
+        | otherwise = stabalize newSeating
+       where
+        newSeating = simulate seating
+      countOccupiedSeats =
+        Vector.sum . Vector.map (Vector.length . Vector.filter (== SeatTile TakenSeat))
+   in countOccupiedSeats . stabalize
+
 main :: IO ()
-main = putStrLn "pending"
+main = do
+  input <- getDay11Input
+  putStrLn $ "Part 1: " <> show (part1 input)
